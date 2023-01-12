7cc274129ab08c331ee2dabaaf44cee16571cfbf
Jogendra Pattnaik <jogendrapattnaik4@gmail.com>
2023-01-12 20:02:13 +0530

    Ip Shown

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import ExhibitContainer from './ExhibitContainer';
 // import ExhibitComponent from './ExhibitComponent';
 import Header from './Header';
+import client from './websocketclient';
 
 function App() {
   return (
--- sextant/src/ExhibitContainer.js
+++ sextant/src/ExhibitContainer.js
@@ -1,11 +1,21 @@
 import React from 'react'
 import ExhibitComponent from './ExhibitComponent'
+import { Component } from 'react';
+import { useState } from 'react';
+
 
 function ExhibitContainer() {
+  const [ipaddr, setIpaddr] = useState("127.0.0.1");
+
+  // componentDidMount(){
+    fetch('https://api.ipify.org?format=json').then((response)=>response.json()).then((data)=>{
+      setIpaddr(String(data.ip))
+      console.log(data.ip)});
+  // }
   return (
     <>
-    <ExhibitComponent heading="Speed" body="100MBPS"/>
-    <ExhibitComponent heading="IP" body="127.0.0.1"/>
+    <ExhibitComponent heading="IP" body={ipaddr} />
+    {/* <ExhibitComponent heading="IP" body="127.0.0.1"/> */}
     </>
   )
 }
